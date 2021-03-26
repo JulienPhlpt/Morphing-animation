6d0974eee2363c59b25bd7cf7393025002366531
Julien Philippot <julien.philippot86@gmail.com>
2021-03-26 16:31:44 +0100

    changing animation

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const moonPath = "M23.5 27.5C23.5 42.6878 27.5 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C27.5 0 23.5 12.3122 23.5 27.5Z";
-const sunPath = "M55 27.5C55 42.6878 42.6878 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C42.6878 0 55 12.3122 55 27.5Z";
+const moonPath =
+  "M23.5 27.5C23.5 42.6878 27.5 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C27.5 0 23.5 12.3122 23.5 27.5Z";
+const sunPath =
+  "M55 27.5C55 42.6878 42.6878 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C42.6878 0 55 12.3122 55 27.5Z";
 
 const darkMode = document.querySelector("#darkMode");
 let toggle = false;
@@ -8,34 +10,57 @@ let toggle = false;
 darkMode.addEventListener("click", () => {
   // anime JS
   const timeline = anime.timeline({
-    duration : 750,
-    esaing : "easeOutExpo"
+    duration: 750,
+    esaing: "easeOutExpo",
   });
-  timeline.add(
-    {
-      targets : ".sun",
-      d : [{ value: toggle ? sunPath : moonPath }]
-    }
-  )
-  .add(
-    {
-      targets : "#darkMode",
-      rotate : -150
-    }, "-=450"
-  )
-  .add(
-    {
-      targets : "section",
-      backgroundColor : toggle ? "rgb(255, 255, 255)" : "rgb(22, 22, 22)",
-      color : toggle ? "rgb(22, 22, 22)" : "rgb(255, 255, 255)"
-    },
-    "-= 720"
-  );
+  timeline
+    .add({
+      targets: ".sun",
+      d: [{ value: toggle ? sunPath : moonPath }],
+    })
+    .add(
+      {
+        targets: "#darkMode",
+        rotate: -150,
+      },
+      "-=450"
+    )
+    .add(
+      {
+        targets: "section",
+        backgroundColor: toggle ? "rgb(255, 255, 255)" : "rgb(22, 22, 22)",
+        color: toggle ? "rgb(22, 22, 22)" : "rgb(255, 255, 255)",
+      },
+      "-= 720"
+    )
+    .add(
+      {
+        targets: ".light",
+        opacity: toggle ? 1 : 0,
+        esaing: "easeOutExpo",
+      },
+      "-= 720"
+    )
+    .add(
+      {
+        targets: ".dark",
+        opacity: toggle ? 0 : 1,
+        esaing: "easeOutExpo",
+      },
+      "-= 720"
+    );
 
   // Switch toggle
-  if(!toggle){
+  if (!toggle) {
     toggle = true;
   } else {
     toggle = false;
   }
 });
+
+// const toggle = document.querySelector("svg");
+// const content = document.querySelector("p");
+
+// toggle.addEventListener("change", () => {
+//   content.classList.toggle("hidden");
+// });
